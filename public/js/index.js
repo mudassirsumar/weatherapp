4c3c4235531ceaf7f9c5c1bb1ee5542df32f6509
Mudassir <mudassirsumar@gmail.com>
2020-05-17 01:57:08 +0500

    Initial Commit

---
+++ public/js/index.js
@@ -0,0 +1,37 @@
+console.log('Client side from javascript file');
+const body = document.querySelector('body')
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+const loader = document.querySelector('#loader')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+   const location = searchElement.value
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
+loader.style.display = 'block'
+
+
+fetch(`http://localhost:3000/weather?address=${location}`)
+
+.then((response) => {
+    response.json()
+    .then((data) => {
+        if (data.error) {
+            loader.style.display = 'none'
+            messageOne.textContent = data.error
+        }
+        else{
+            loader.style.display = 'none'
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=ba54d553d011873d41486999f334c616&units=metric`
+
+request ({url, json: true}, (error, response) => {
+    if (error) {
+        callback('Unable to connect to Weather API', undefined)
+    }
+    else if (response.body.cod == 400){    
+        callback('Unable to find your location', undefined)
+    }
+    else {
+        callback(undefined, `Description: ${response.body.current.weather[0].description} The temperature is ${response.body.current.temp}℃. There is ${response.body.current.clouds}% chance of rain`)
+    }
+
+})
+
+}
+
+
+module.exports = forecast
